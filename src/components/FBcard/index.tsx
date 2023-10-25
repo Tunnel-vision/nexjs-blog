46b7d975deef8a9390eb87316f57c0e9c5a5e447
tunnel-vision <1002993703@qq.com>
2023-10-25 22:36:32 +0800

    fix: update code

--- src/components/FBcard/index.tsx
+++ src/components/FBcard/index.tsx
@@ -3,7 +3,7 @@ import { FacebookEmbed } from 'react-social-media-embed'
 
 const FBCard = ( ) => {
     return <div>
-    <FacebookEmbed url="https://www.facebook.com/JessicaZanginChina/posts/pfbid02X6JsKgWeuuLxNzdSWsjk2ivgpxAWehWsNHjuu1Lkdy489T4ggFRnhrztuZFnfVAKl" width={325}  />
+    <FacebookEmbed url="https://www.facebook.com/JessicaZanginChina/posts/pfbid02X6JsKgWeuuLxNzdSWsjk2ivgpxAWehWsNHjuu1Lkdy489T4ggFRnhrztuZFnfVAKl" width={325} height={'100%'}  />
   </div>
 }
 
